b8a0d29d756d44d34ef5bbb3cdbaac662e160ac2
manuel milillo <manuelmilillo@gmail.com>
2024-05-21 02:11:21 +0200

    fix tests

--- src/command/command.controller.spec.ts
+++ src/command/command.controller.spec.ts
@@ -1,18 +1,26 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { CommandController } from './command.controller';
+import { CommandService } from './command.service';
 
 describe('CommandController', () => {
   let controller: CommandController;
+  let service: CommandService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [CommandController],
+      providers: [CommandService]
     }).compile();
 
     controller = module.get<CommandController>(CommandController);
+    service = module.get<CommandService>(CommandService);
   });
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  it('should be defined', () => {
+    expect(service).toBeDefined();
   });
 });
--- src/yaml-generator/yaml-generator.controller.spec.ts
+++ src/yaml-generator/yaml-generator.controller.spec.ts
@@ -1,18 +1,26 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { YamlGeneratorController } from './yaml-generator.controller';
+import { YamlGeneratorService } from './yaml-generator.service';
 
 describe('YamlGeneratorController', () => {
   let controller: YamlGeneratorController;
+  let service: YamlGeneratorService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [YamlGeneratorController],
+      providers: [YamlGeneratorService]
     }).compile();
 
+    service = module.get<YamlGeneratorService>(YamlGeneratorService)
     controller = module.get<YamlGeneratorController>(YamlGeneratorController);
   });
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  it('should be defined', () => {
+    expect(service).toBeDefined();
   });
 });
